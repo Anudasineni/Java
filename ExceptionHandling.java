b5b4c5f57e57695d8843b6cb27eadeeea8a24dd1
Anusha <101965726+Anudasineni@users.noreply.github.com>
2022-03-22 18:26:58 +0530

    Update ExceptionHandling.java

--- ExceptionHandling.java
+++ ExceptionHandling.java
@@ -1,6 +1,6 @@
 public class ExceptionHandling {
     public static void main(String args[]) {
-        int num1 = 0;
+        int num1 = 10;
         int num2 = 0;
         try {
         /*If an exception occurs in try block then the control of  is passed to the corresponding catch block.
@@ -11,33 +11,35 @@ public class ExceptionHandling {
 
            // int n[]=new int[4];
            // System.out.println(n[5]=30);Warning: ArrayIndexOutOfBoundsException
-            System.out.println(num1 / num2);//if there is no error in try block ,then catch block will not execute.
+            System.out.println(num1 / num2);// //statement1.if there is no error in try block ,then catch block will not execute.
 
 /* note:when ever exception is found in try block,control will be passed to the catch block.So the Statements which are there in try-block
 *after the exception will not execute.example
  */
-            System.out.println(num2);//this will not execute.because exception is already found at 14.
+            System.out.println(num1);//statement2.this will not execute,because exception is already found at 14.
 
         }
         catch (ArithmeticException e)//this catch block will handle only if any ArithmeticException occurs in try block.
         {
-            System.out.println(e);
-            System.out.println(num1 + num2);
+            System.out.println(e);//statement3
+            System.out.println(num1 + num2);//statement4
 
         }
         catch (ArrayIndexOutOfBoundsException e1){//this catch block will handle only if any ArrayIndexOutOfBoundsException
           // occurs in try block.
-            System.out.println("Warning: ArrayIndexOutOfBoundsException");
+            System.out.println("Warning: ArrayIndexOutOfBoundsException");//statement5
 
         }
         catch (Exception e2){//this catch block handles all the exceptions.
-            System.out.println("Warning: some exception.");
+            System.out.println("Warning: some exception.");//statement6
 
         }
         finally {
-            System.out.println("out of try block");
+            System.out.println("finally block");//statement7
         }
-        System.out.println(num1*num2);
+        System.out.println(num1*num2);//statement8  
+        
+        /*Statement 3 will print exception message.statement4,statement7,statement8 will execute.
     }
 
 }
