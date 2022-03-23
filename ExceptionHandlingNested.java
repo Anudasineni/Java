a328f884c4af9efa4224ddf75ac47b8aef0e5c9b
Anusha <101965726+Anudasineni@users.noreply.github.com>
2022-03-23 13:39:51 +0530

    Update ExceptionHandlingNested.java

--- ExceptionHandlingNested.java
+++ ExceptionHandlingNested.java
@@ -64,6 +64,57 @@ public class ExceptionHandlingNested {
         System.out.println("outer final block");
 
     }
+//try-catch finally block in nested try.
+     try {//main try-block
+
+         try {//try-block1
+             try { //try-block2
+                 System.out.println("try-block2");
+
+                 int[] arr=new int[3];
+                 arr[6]=10;
+             } catch (ArithmeticException e) {
+                 System.out.println("ArithmeticException");
+                 System.out.println("handled in try-block2");
+
+             } 
+             finally {
+                 System.out.println("final try-block2" );
+             }
+         }
+         catch (ArithmeticException e1) {
+             System.out.println("ArithmeticException");
+             System.out.println("handeled in try-block1");
+
+         } 
+         finally {
+             System.out.println("final of try-block1");
+         }
+     }
+     catch (ArithmeticException e2){
+         System.out.println("ArithmeticException");
+         System.out.println("handled in main try-block");
+
+     }
+     catch (ArrayIndexOutOfBoundsException e3){
+         System.out.println("ArrayIndexBoundException");
+         System.out.println("handled in main try-block");
+
+     }
+     catch (Exception e4){
+         System.out.print("Exception");
+         System.out.println("handled in main try-block");
+
+     }
+     finally {
+         System.out.println("final main try-block");
+     }
+        System.out.print("next");
+/* ArrayIndexOutOfBoundsException occurred in the  try-block2.Since try-block2 is not handling this exception,
+the control then gets transferred to the parent try-block1 and looked for the catch handlers in try-block1.
+ Since the try-block1 is also not handling that exception,the control gets transferred to the main  try-block
+where it found the suited catch block(ArrayIndexBoundException) for exception.This is how the nesting structure works.
+ */
 
     }
 }
